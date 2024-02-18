05e89a9afda5166f88c783fcbd9ae53eb4c83291
devjahid1 <developerjahid1@gmail.com>
2024-02-18 16:57:47 +0600

    l

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -1,12 +1,33 @@
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase()
+
+    if(playerPressed === expectedAlphabet){
+        console.log('you get a point');
+        console.log('You have pressed correctly', expectedAlphabet);
+        removeBackgroundColorById(expectedAlphabet)
+        continueGame();
+    }
+    else{
+        console.log('you lost a life');
+    }
+}
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
+
+
+
 function continueGame(){
-   const alphabet= getARendomAlphabet();
-   console.log('your random alphabet', alphabet);
-   const currentAlphabetElement = document.getElementById('current-alphabet');
+    const alphabet = getARendomAlphabet();
+    console.log(alphabet);
+    const currentAlphabetElement= document.getElementById('current-alphabet');
     currentAlphabetElement.innerText=alphabet;
     setBackgroundColor(alphabet);
 }
 function play(){
     hiddenElementById('home-screen');
-    showElementById('play-ground')
+    showElementById('play-ground');
     continueGame();
 }
--- scripts/utility.js
+++ scripts/utility.js
@@ -4,17 +4,21 @@ function hiddenElementById(elementId){
 }
 function showElementById(elementId){
     const element = document.getElementById(elementId)
-    element.classList.remove('hidden')
-}
-function setBackgroundColor(elementId){
-    const element= document.getElementById(elementId);
-    element.classList.add('bg-orange-400')
+    element.classList.remove('hidden');
 }
 function getARendomAlphabet(){
-    const alphabetString = 'abcdefghijklmnopqrstuvwxyz';
+    const alphabetString = 'abcdefghijklmnopqrstwxyz';
     const alphabets = alphabetString.split('');
     const randomNumber = Math.random()*25;
-    const index=Math.round(randomNumber);
+    const index = Math.round(randomNumber);
     const alphabet = alphabets[index];
-     return alphabet;
+    return alphabet;
+}
+function setBackgroundColor(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.add('bg-orange-400');
+}
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId)
+    element.classList.remove('bg-orange-400');
 }
