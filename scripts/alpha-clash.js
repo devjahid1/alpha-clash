33d7ed09f4c5d1465773d218b66dd4b6e9343738
devjahid1 <developerjahid1@gmail.com>
2024-02-18 17:29:17 +0600

    Score & life event

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -8,11 +8,23 @@ function handleKeyboardKeyUpEvent(event){
     if(playerPressed === expectedAlphabet){
         console.log('you get a point');
         console.log('You have pressed correctly', expectedAlphabet);
+        //Count Score
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        console.log((currentScore));
+        const newScore = currentScore + 1;
+        currentScoreElement.innerText = newScore;
         removeBackgroundColorById(expectedAlphabet)
         continueGame();
     }
     else{
         console.log('you lost a life');
+        const currentLifeElement=document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText)
+        const newLife = currentLife -1;
+        currentLifeElement.innerText = newLife
     }
 }
 document.addEventListener('keyup', handleKeyboardKeyUpEvent);
